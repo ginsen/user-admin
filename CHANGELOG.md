df762eb7bd50c3b95cd95cc675b3783019fcea1f
J. Ginés Hernández G <jgines@yaencontre.com>
2019-07-24 17:27:50 +0200

    Install others require-devs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+ * Install others require-devs.
  * Install PHPUnit Testing framework.
  * Install sensiolabs-de/deptrac-shim and configure.
  * Install friendsofphp/php-cs-fixer and fixed code.
--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,10 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
