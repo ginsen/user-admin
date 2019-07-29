e730d4e917433791519d44035a0bf479e2cee88c
J. Gines Hernandez G <jgines@gmail.com>
2019-07-29 15:56:16 +0200

    Install broadway/broadway and dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Broadway\Bundle\BroadwayBundle\BroadwayBundle::class => ['all' => true],
 ];
