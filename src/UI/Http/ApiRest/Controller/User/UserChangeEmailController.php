626db0edca0faae654f7226dc5079a91685fccf8
J. Gines Hernandez G <jgines@gmail.com>
2019-08-05 14:54:10 +0200

    disable JWT validator

--- src/UI/Http/ApiRest/Controller/User/UserChangeEmailController.php
+++ src/UI/Http/ApiRest/Controller/User/UserChangeEmailController.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace App\UI\Http\ApiRest\Controller\User;
 
 use App\Application\Command\User\ChangeEmail\ChangeEmailCommand;
-use App\Infrastructure\Exception\Session\ForbiddenException;
-use App\Infrastructure\User\Authentication\Session;
 use App\UI\Http\ApiRest\Controller\CommandQueryController;
 use Assert\Assertion;
-use League\Tactician\CommandBus;
-use Nelmio\ApiDocBundle\Annotation\Security;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,22 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserChangeEmailController extends CommandQueryController
 {
-    /** @var Session */
-    private $session;
-
-    public function __construct(Session $session, CommandBus $commandBus, CommandBus $queryBus)
-    {
-        $this->session = $session;
-
-        parent::__construct($commandBus, $queryBus);
-    }
-
-
     /**
      * @Route(
      *     "/user/{uuid}/email",
      *     name="api_user_change_email",
-     *     methods={"POST"}
+     *     methods={"PUT"}
      * )
      *
      * @SWG\Response(
@@ -64,17 +49,14 @@ class UserChangeEmailController extends CommandQueryController
      *
      * @SWG\Tag(name="User")
      *
-     * @Security(name="Bearer")
-     *
      * @param string  $uuid
      * @param Request $request
      * @return JsonResponse
      */
     public function __invoke(string $uuid, Request $request): JsonResponse
     {
-        $this->validateUuid($uuid);
-
-        $email = $request->get('email');
+        $params = json_decode($request->getContent(), true);
+        $email  = $params['email'];
 
         Assertion::notNull($email, "Email can\'t be null");
 
@@ -83,16 +65,5 @@ class UserChangeEmailController extends CommandQueryController
         $this->handleCommand($command);
 
         return JsonResponse::create();
-    }
-
-
-    /**
-     * @param string $uuid
-     */
-    private function validateUuid(string $uuid): void
-    {
-        if (!$this->session->sameByUuid($uuid)) {
-            throw new ForbiddenException();
-        }
     }
 }
