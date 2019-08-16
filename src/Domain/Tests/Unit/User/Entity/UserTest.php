c104d0fa9cfa3d33fd9bdf47ecf18ee3fb2cbaa7
J. Gines Hernandez Galindo <jgines@gmail.com>
2019-08-16 19:04:23 +0200

    add new test to covert user aggregates

--- src/Domain/Tests/Unit/User/Entity/UserTest.php
+++ src/Domain/Tests/Unit/User/Entity/UserTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace App\Domain\Tests\Unit\User\Entity;
 
 use App\Domain\User\Entity\User;
+use App\Domain\User\Exception\Email\EmailAlreadyExistException;
 use App\Domain\User\Specification\UniqueEmailSpecificationInterface;
 use App\Domain\User\ValueObj\Credentials;
-use App\Domain\User\ValueObj\DateTime;
 use App\Domain\User\ValueObj\Email;
 use App\Domain\User\ValueObj\Password;
 use PHPUnit\Framework\TestCase;
@@ -15,11 +15,22 @@ use Ramsey\Uuid\Uuid;
 
 class UserTest extends TestCase implements UniqueEmailSpecificationInterface
 {
-    protected $isUnique = true;
+    /** @var bool */
+    protected $isUnique;
+
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $this->isUnique = true;
+    }
 
 
     /**
      * @test
+     * @throws \Exception
      */
     public function it_should_create_one_user_instance()
     {
@@ -40,10 +51,93 @@ class UserTest extends TestCase implements UniqueEmailSpecificationInterface
 
 
     /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_should_return_exception_if_email_is_not_unique()
+    {
+        $this->isUnique = false;
+        $this->expectException(EmailAlreadyExistException::class);
+
+        $uuid = Uuid::uuid4();
+        $credentials = new Credentials(
+            Email::fromStr('test@test.net'),
+            Password::encode('123456')
+        );
+
+        User::create($uuid, $credentials, $this);
+    }
+
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_should_sign_in_user()
+    {
+        $uuid = Uuid::uuid4();
+        $credentials = new Credentials(
+            Email::fromStr('sign.in@test.net'),
+            Password::encode('123456')
+        );
+
+        $user = User::create($uuid, $credentials, $this);
+        $user->signIn('123456');
+
+        self::assertInstanceOf(User::class, $user);
+    }
+
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_should_change_email_user()
+    {
+        $uuid = Uuid::uuid4();
+        $credentials = new Credentials(
+            Email::fromStr('foo@test.net'),
+            Password::encode('123456')
+        );
+
+        $user = User::create($uuid, $credentials, $this);
+        $user->changeEmail(Email::fromStr('bar@test.net'), $this);
+
+        self::assertEquals('bar@test.net', $user->getEmail()->toStr());
+    }
+
+
+    /**
+     * @test
+     * @throws \Exception
+     */
+    public function it_should_change_alive_status_user()
+    {
+        $uuid = Uuid::uuid4();
+        $credentials = new Credentials(
+            Email::fromStr('active@test.net'),
+            Password::encode('123456')
+        );
+
+        $user = User::create($uuid, $credentials, $this);
+
+        $user->changeActive(false);
+        self::assertFalse($user->isActive());
+
+        $user->changeActive(true);
+        self::assertTrue($user->isActive());
+    }
+
+
+    /**
      * {@inheritDoc}
      */
     public function isUnique(Email $email): bool
     {
+        if (!$this->isUnique) {
+            throw new EmailAlreadyExistException('Email already registered.');
+        }
+
         return $this->isUnique;
     }
 }
